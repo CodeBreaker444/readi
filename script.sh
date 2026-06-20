59957ac4830da8fa41c2a49892081e32c99b2c79
Govardhan Chitrada <CodeBreaker444@users.noreply.github.com>
2026-06-20 20:34:30 +0530

    Refactor script.sh for improved readability

--- script.sh
+++ script.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
-
 if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" || \
       "$VERCEL_GIT_COMMIT_REF" == "main" || \
       "$VERCEL_GIT_COMMIT_REF" == "feature/prisma" ]]; then
-
   echo "✅ Build can proceed"
   exit 1   # Continue build
-
 else
-
   echo "🛑 Build cancelled"
   exit 0   # Skip build
-
 fi
