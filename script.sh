c194b7cd2f96bfb53750e268438bed446159a993
Govardhan Chitrada <CodeBreaker444@users.noreply.github.com>
2026-06-20 20:40:05 +0530

    Fix script exit codes for build conditions

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" || "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "feature/prisma" ]]; then
   echo "✅ Build can proceed"
-  exit 1   # Continue build
+  exit 1
 else
   echo "🛑 Build cancelled"
-  exit 0   # Skip build
+  exit 0
 fi
