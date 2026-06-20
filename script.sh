59ebdc63f5b239ef8a81188f961b5d27be25235c
Govardhan Chitrada <CodeBreaker444@users.noreply.github.com>
2026-06-20 20:32:39 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,13 +2,16 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" || "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "feature/prisma" ]] ; then
-  # Proceed with the build
-    echo "✅ - Build can proceed"
-  exit 1;
+if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" || \
+      "$VERCEL_GIT_COMMIT_REF" == "main" || \
+      "$VERCEL_GIT_COMMIT_REF" == "feature/prisma" ]]; then
+
+  echo "✅ Build can proceed"
+  exit 1   # Continue build
 
 else
-  # Don't build
-  echo "🛑 - Build cancelled"
-  exit 0;
+
+  echo "🛑 Build cancelled"
+  exit 0   # Skip build
+
 fi
